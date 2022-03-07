1702f9c4b68245a75f6c9f4118bfc4f2f856e542
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-07 16:12:54 +0100

    Give name a default value of unknown

--- person.rb
+++ person.rb
@@ -1,5 +1,5 @@
 class Animal
-  def initialize(name , age, parent_permission)
+  def initialize(name ="Unknown", age, parent_permission)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
