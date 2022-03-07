6bc894fa75287d1ddeba54da97b50bf48a2ebcf8
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-07 17:46:38 +0100

    Make Corrections detected by rubocop

--- person.rb
+++ person.rb
@@ -1,25 +1,27 @@
 # frozen_string_literal: true
 
+# Person Class
 class Person
   def initialize(age,
-     name = 'Unknown', 
-    _parent_permission = true
-  )
+                 name = 'Unknown',
+                 _parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+  end
 
-    attr_accessor :name, :age
-    attr_reader :id
+  attr_accessor :name, :age
+  attr_reader :id
 
-    def is_of_age?
-      age >= 18
-    end
+  # rubocop:disable Naming/PredicateName
+  def is_of_age?
+    age >= 18
+  end
 
-    private :is_of_age?
+  private :is_of_age?
 
-    def can_use_services?
-      is_of_age? || parent_permission ? true : false
-    end
+  # rubocop:enable Naming/PredicateName
+  def can_use_services?
+    is_of_age? || parent_permission ? true : false
   end
 end
