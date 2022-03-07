5146b2342cf3afb3ba54c522774144b36d1e8ab7
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-07 20:47:47 +0100

    Fix Person class

--- person.rb
+++ person.rb
@@ -5,19 +5,20 @@ class Person
 
   def initialize(age,
                  name = 'Unknown',
-                 _parent_permission: true)
+                 parent_permission = true)
     @id = Random.rand(1..1000)
-    @name = name
     @age = age
+    @name = name
+    @parent_permission = parent_permission
   end
 
   def can_use_services?
-    of_age? || parent_permission ? true : false
+    of_age? || @parent_permission ? true : false
   end
 
   private
 
   def of_age?
-    age >= 18
+    age.to_i >= 18.to_i
   end
 end
