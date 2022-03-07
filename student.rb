dc8d4905d638394ade4488c8cfc2d38becc4f816
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-07 18:27:38 +0100

    Add name to Student contructor

--- student.rb
+++ student.rb
@@ -1,12 +1,18 @@
 require './person'
 # Student Class Inheriting From Person Class
 class Student < Person
-  def initialize(classroom)
+  def initialize(classroom, name)
     super('student')
     @classroom = classroom
+    @name = name
   end
 
   def play_hooky
-    "¯\(ツ)/¯"
+   "¯\(ツ)/¯"
   end
 end
+
+student = Student.new("class1", "Victor")
+
+puts student.play_hooky()
+puts student.name
