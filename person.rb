482f866967285c4ed68009608788a8442b64b2e1
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-07 17:29:07 +0100

    Break down constructor into different lines

--- person.rb
+++ person.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class Person
-  def initialize(age, name = 'Unknown', _parent_permission = true)
+  def initialize(age,
+     name = 'Unknown', 
+    _parent_permission = true
+  )
     @id = Random.rand(1..1000)
     @name = name
     @age = age
