34b3cc5d12cf4c1b66dd60104baa20e044a17861
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-08 04:49:56 +0100

    Use keyword arguments for arguments with a boolean default

--- person.rb
+++ person.rb
@@ -5,7 +5,7 @@ class Person
 
   def initialize(age,
                  name = 'Unknown',
-                 parent_permission = true)
+                 parent_permission: true)
     @id = Random.rand(1..1000)
     @age = age
     @name = name
