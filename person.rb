73119835270f27264fd5afa13f6cee60e20c67b4
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-10 16:22:20 +0100

    Add all methods and classes for school library secorator

--- person.rb
+++ person.rb
@@ -1,11 +1,10 @@
 # Person Class
-import "./namable"
+require './namable'
 
 class Person < Namable
-  def initialize(name)
-    # assign a namable object from params to an instance variable
-    super(name)
-    @name = name
+  # add correct_name method that returns the name attribute.
+  def correct_name
+    @name
   end
 
   attr_accessor :name, :age
@@ -14,6 +13,7 @@ class Person < Namable
   def initialize(age,
                  name = 'Unknown',
                  parent_permission: true)
+    super(name, age, parent_permission: true)
     @id = Random.rand(1..1000)
     @age = age
     @name = name
@@ -28,5 +28,44 @@ class Person < Namable
 
   def of_age?
     age.to_i >= 18.to_i
+  end
+end
+
+# In the constructor assign a namable object from params to an instance variable.
+class Decorator < Namable
+  def initialize(component)
+    super(component)
+    @component = component
+  end
+
+  # Implement the correct_name method that returns the result of the correct_name method of the @namable
+  def correct_name
+    @name.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def initialize(component)
+    super(component)
+    @component = component
+  end
+
+  # Implement a method correct_name that capitalizes the output of @nameable.correct_name
+  def correct_name
+    @component.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def initialize(component)
+    super(component)
+    @component = component
+  end
+
+  # Implement a method correct_name that makes
+  # sure that the output of @nameable.correct_name
+  # has a maximum of 10 characters.
+  def correct_name
+    @component.correct_name[0..9]
   end
 end
