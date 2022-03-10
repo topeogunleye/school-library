913dbe4c96f73be6d313327fe39f8e3c9b851e6a
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-11 00:22:11 +0100

    Remove unneccesary comments

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Create methods that do the following:
 # List all books.
 # List all people.
@@ -6,8 +8,8 @@
 # Create a rental.
 # List all rentals for a given person id.
 
-require "./classroom"
-require "./person"
+require './classroom'
+require './person'
 
 def list_books
   Book.all.each do |book|
@@ -22,45 +24,40 @@ def list_people
 end
 
 def create_person(type)
-  if type == "teacher"
-    Teacher.new("Pysics", 20, "Jon Snow", parent_permission: false)
-  elsif type == "student"
-    Student.new("Class1", 19, "Trent", parent_permission: false)
+  case type
+  when 'teacher'
+    Teacher.new('Pysics', 20, 'Jon Snow', parent_permission: false)
+  when 'student'
+    Student.new('Class1', 19, 'Trent', parent_permission: false)
   end
 end
 
 def create_book
-  Book.new("Harry Potter", "JK Rowling")
+  Book.new('Harry Potter', 'JK Rowling')
 end
 
 def create_rental(date, book, person)
   Rental.new(date, book, person)
 end
 
-def list_rentals(person_id)
+def list_rentals(_person_id)
   list_books
   list_people
-  create_person("teacher")
+  create_person('teacher')
   create_book
-  create_rental("10/10/10", create_book, create_person("student"))
+  create_rental('10/10/10', create_book, create_person('student'))
 end
 
-# define the entry point, this will be a method called main that is invoked at the end of your file. This method should do the following:
-#   Present the user with a list of options to perform.
-#   Lets users choose an option.
-#   If needed, ask for parameters for the option.
-#   Have a way to quit the app.
-
 def main
-  puts "Welcome to the Library App"
-  puts "What would you like to do?"
-  puts "1. List Books"
-  puts "2. List People"
-  puts "3. Create a person"
-  puts "4. Create a book"
-  puts "5. Create a rental"
-  puts "6. List all rentals for a given person id"
-  puts "7. Quit"
+  puts 'Welcome to the Library App'
+  puts 'What would you like to do?'
+  puts '1. List Books'
+  puts '2. List People'
+  puts '3. Create a person'
+  puts '4. Create a book'
+  puts '5. Create a rental'
+  puts '6. List all rentals for a given person id'
+  puts '7. Quit'
 
   choice = gets.chomp.to_i
 
@@ -70,22 +67,22 @@ def main
   when 2
     list_people
   when 3
-    puts "What type of person would you like to create?"
-    puts "1. Teacher"
-    puts "2. Student"
+    puts 'What type of person would you like to create?'
+    puts '1. Teacher'
+    puts '2. Student'
     type = gets.chomp.to_i
-    create_person("teacher") if type == 1
-    create_person("student") if type == 2
+    create_person('teacher') if type == 1
+    create_person('student') if type == 2
   when 4
     create_book
   when 5
-    create_rental("10/10/10", create_book, create_person("student"))
+    create_rental('10/10/10', create_book, create_person('student'))
   when 6
     list_rentals(1)
   when 7
-    puts "Goodbye!"
+    puts 'Goodbye!'
   else
-    puts "Invalid choice"
+    puts 'Invalid choice'
   end
 
   main
