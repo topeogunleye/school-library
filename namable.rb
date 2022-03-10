8c0bfc4f040745ba87167bda6e75dc8fd6c2ed45
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-10 12:51:47 +0100

    Implement a method in class Namable called correct_name that will raise a NotImplementedError

--- namable.rb
+++ namable.rb
@@ -3,5 +3,10 @@ class Namable
   def initialize(name)
     @name = name
   end
+  
+  # Implement a method called correct_name that will raise a NotImplementedError
+  def correct_name
+    raise NotImplementedError
+  end
 
 end
