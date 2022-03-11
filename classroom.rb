87134b42fa0eabc2d6ce3ec5b85b1b6cd4ae5fed
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-11 17:58:57 +0100

    Remove commented out code

--- classroom.rb
+++ classroom.rb
@@ -1,9 +1,4 @@
-# Create the has-many/belongs-to relationship between Classroom and Student. The following should be implemented:
-# Create the has-many side (a classroom has many students).
-# Create the belongs-to side (a student belongs to a classroom).
-# Make sure that when adding a student to a classroom it also sets the classroom for the student.
-# Make sure that when setting the classroom for a student it also adds it to the classrooms' students.
-
+# classroom, book and rental classes are defined
 require './student'
 require './person'
 
