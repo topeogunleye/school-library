2e93054a5237b2d16dac7a753ae5d3d8714f1be3
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-13 05:31:58 +0100

    Remove rental class and book class from classroom

--- classroom.rb
+++ classroom.rb
@@ -15,33 +15,3 @@ class Classroom
     student.classroom = self
   end
 end
-
-class Book
-  attr_accessor :title, :author, :rentals
-
-  def initialize(title, author)
-    @title = title
-    @author = author
-    @rentals = []
-  end
-
-  def add_rental(rental)
-    @rentals.push(rental)
-    rental.book = self
-  end
-end
-
-# Create a class Rental with the following:
-# @date instance variable, should be initialized in the constructor.
-# Setter and getter for @date (remember about attr_accessor).
-# Modify the constructor of Rental so Book and Person are set in it.
-
-class Rental
-  attr_accessor :date, :book, :person
-
-  def initialize(date, book, person)
-    @date = date
-    @book = book
-    @person = person
-  end
-end
